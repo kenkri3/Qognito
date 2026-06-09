d38877c35f64bf5f58185c0a51c4a7a29c684538
kenkri3 <github@qognito.no>
2026-06-09 16:05:43 +0800

    Debug: forenklet app uten router

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router'
 import './index.css'
 import Home from './pages/Home'
-import Personvern from './pages/Personvern'
-import Vilkar from './pages/Vilkar'
-
-const router = createHashRouter([
-  { path: '/', element: <Home /> },
-  { path: '/personvern', element: <Personvern /> },
-  { path: '/vilkar', element: <Vilkar /> },
-])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </StrictMode>,
 )
