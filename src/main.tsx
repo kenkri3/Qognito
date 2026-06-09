71bc0f1552c2298e7f215985d346d821333b656a
kenkri3 <github@qognito.no>
2026-06-09 16:03:15 +0800

    Lagt til CORS-headers og fikset React Router imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router'
 import './index.css'
 import Home from './pages/Home'
 import Personvern from './pages/Personvern'
