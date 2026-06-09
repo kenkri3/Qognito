7a977ebdb17f7fe4fe297d6aabb01e1f0490bf65
kenkri3 <github@qognito.no>
2026-06-09 16:07:24 +0800

    Debug: hello world test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <div style={{ padding: 50, fontSize: 30, color: 'black' }}>
+      <h1>Qognito fungerer!</h1>
+      <p>Hvis du ser denne teksten, fungerer React.</p>
+    </div>
   </StrictMode>,
 )
